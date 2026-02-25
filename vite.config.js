b7a9d5ccfce9ad32c103bcc11826dfa81fd20eb9
Laurenz Szepan <laurenz.kurek@me.com>
2026-02-25 09:01:17 +0100

    "Deploy"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// WICHTIG: Ersetze 'portfolio' mit deinem GitHub Repository-Namen
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
